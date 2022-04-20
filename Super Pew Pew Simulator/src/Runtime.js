f943df4e31435b608ea737f908bc7ceb8e6a5572
Str4ky <straky@outlook.fr>
2022-04-20 11:08:18 +0200

    Edit game

--- Super Pew Pew Simulator/src/Runtime.js
+++ Super Pew Pew Simulator/src/Runtime.js
@@ -45430,7 +45430,7 @@ window['Runtime'] = (function Runtime(__can, __path){
 
 	    createList: function (file) {
 
-	        var extMaxHandle = 1;
+	        var extMaxHandle = 0;
 	        if (extMaxHandle) {
 	            this.extensions = new Array(extMaxHandle);
 	            this.numOfConditions = new Array(extMaxHandle);
